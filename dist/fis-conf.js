bc1ed6e9fe5f01c669df0e31b5f785c5426ed0a0
九零 <zhangyuteng1029@163.com>
2015-06-20 14:59:54 +0800

    后勤处首版

---
+++ dist/fis-conf.js
@@ -0,0 +1,30 @@
+// fis.config.merge({
+//     roadmap : {
+//         path : [
+//             {
+//                 //所有的js文件
+//                 reg : '**.js',
+//                 //发布到/static/js/xxx目录下
+//                 release : '/$&',
+//                 //访问url是/mm/static/js/xxx
+//                 url : '.$&'
+//             },
+//             {
+//                 //所有的css文件
+//                 reg : '**.css',
+//                 //发布到/static/css/xxx目录下
+//                 release : '/$&',
+//                 //访问url是/pp/static/css/xxx
+//                 url : '.$&'
+//             },
+//             {
+//                 //所有的css文件
+//                 reg : '**.jpg',
+//                 //发布到/static/css/xxx目录下
+//                 release : '/$&',
+//                 //访问url是/pp/static/css/xxx
+//                 url : '.$&'
+//             }
+//         ]
+//     }
+// });
